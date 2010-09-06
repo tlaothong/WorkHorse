5bb8e5e17fd38053300a6e5e3b9e00e6ef818e5d
tlaothong <tlaothong@gmail.com>
2010-09-06 12:39:22 +0700

    Add PlainUnitTest.BasicStub demo.
    Signed-off-by: tlaothong <tlaothong@gmail.com>

---
+++ src/SpecTech/MockAndStub/PerfEx.Demo.SystemUnderTest/SystemUnderTest/IWorkTogether.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PerfEx.Demo.SystemUnderTest
+{
+    public interface IWorkTogether
+    {
+        string ReadText();
+        int NumberOfFans { get; }
+        string NumToText(int number);
+    }
+}
