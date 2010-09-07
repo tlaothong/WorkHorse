80b39316e44d954246edffbe5934b1b44df41a04
tlaothong <tlaothong@gmail.com>
2010-09-07 16:08:56 +0700

    Create an excercise ExcerciseSpecTableToRound in SpecTech.

---
+++ src/SpecTech/ExcerciseSpecTableToRound/PerfEx.Demo.SimpleGame.Commands/Commands/ListActiveGameRoundsCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PerfEx.Demo.SimpleGame.Models;
+
+namespace PerfEx.Demo.SimpleGame.Commands
+{
+    public class ListActiveGameRoundsCommand
+    {
+        public DateTime FromTime { get; set; }
+        public IEnumerable<GameRound> Rounds { get; set; }
+    }
+}
