c666c832b9a0ef39d0cdb9de57ecfcc7c45c76e7
nittaya <nittaya@perfenterprise.com>
2010-09-14 18:44:03 +0700

    AddComment

--- src/CasinovaColors/BlackAndWhite/TheS.Casinova.ColorsGame.Commands/Commands/PayForColorsWinnerInfoCommand.cs
+++ src/CasinovaColors/BlackAndWhite/TheS.Casinova.ColorsGame.Commands/Commands/PayForColorsWinnerInfoCommand.cs
@@ -5,12 +5,15 @@ using System.Text;
 
 namespace TheS.Casinova.ColorsGame.Commands
 {
+    
     public class PayForColorsWinnerInfoCommand
     {
+        //Generate TrackID input
         public string UserName { get; set; }
         public int TableID { get; set; }
         public int RoundID { get; set; }
 
+        //Generate TrackID output
         public Guid TrackingID { get; set; }
     }
 }
