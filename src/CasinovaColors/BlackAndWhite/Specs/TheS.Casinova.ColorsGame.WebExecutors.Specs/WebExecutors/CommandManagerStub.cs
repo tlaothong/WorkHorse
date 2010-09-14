77dad12aa2c5e4b9191ae2dbc8d9811025d25a88
Teerachai <tlaothong@gmail.com>
2010-09-14 16:52:37 +0700

    Add CasinovaColors/BlackAndWhite
    Signed-off-by: Teerachai <tlaothong@gmail.com>

---
+++ src/CasinovaColors/BlackAndWhite/Specs/TheS.Casinova.ColorsGame.WebExecutors.Specs/WebExecutors/CommandManagerStub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PerfEx.Infrastructure;
+
+namespace TheS.Casinova.ColorsGame.WebExecutors
+{
+    public class CommandManagerStub : PerfEx.Infrastructure.CommandPattern.CommandManagerBase
+    {
+        // TODO: Add Type refereces
+
+        private Action<PerfEx.Infrastructure.IDependencyRegistry> _registerAction;
+
+        public CommandManagerStub(Action<PerfEx.Infrastructure.IDependencyRegistry> registerAction)
+            : base(new PerfEx.Infrastructure.Containers.StructureMapAdapter.StructureMapAbstractFactory())
+        {
+            _registerAction = registerAction;
+        }
+
+        protected override void InitCommandRegistry(PerfEx.Infrastructure.CommandPattern.CommandExecutorRegistry registry)
+        {
+            RegisterReferenceExecutors(registry, "web");
+
+            InitRegistry((PerfEx.Infrastructure.IDependencyRegistry)registry);
+        }
+
+        protected virtual void InitRegistry(PerfEx.Infrastructure.IDependencyRegistry registry)
+        {
+            _registerAction(registry);
+        }
+    }
+}
---
+++ src/CasinovaColors/BlackAndWhite/TheS.Casinova.ColorsGame.DAL.BackContracts/DAL/IColorsGameDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PerfEx.Infrastructure.Data;
+using TheS.Casinova.ColorsGame.Models;
+using TheS.Casinova.ColorsGame.Commands;
+
+namespace TheS.Casinova.ColorsGame.DAL
+{
+    public interface IColorsGameDataAccess :
+        IPayForColorsWinnerInfo,
+        IUpdateWinnerToGamePlayInfo
+    { }
+
+    public interface IPayForColorsWinnerInfo : IDataAction<PlayerInformation, PayForColorsWinnerInfoCommand>
+    { }
+
+    public interface IUpdateWinnerToGamePlayInfo : IDataAction<GamePlayInformation, PayForColorsWinnerInfoCommand>
+    { }
+}
