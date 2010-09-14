e665ba7ad500a4fea9140d449668dbca7e11abbd
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2010-09-14 19:15:42 +0700

    create silverlight test project specs
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ src/CasinovaColors/BlackAndWhite/TheS.Casinova.ColorsGame.Specs/Specs/Steps/CommonSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using TechTalk.SpecFlow;
+using Rhino.Mocks;
+
+namespace TheS.Casinova.ColorsGame.Specs.Specs.Steps
+{
+    [Binding]
+    public class CommonSteps
+    {
+        MockRepository Mocks { get { return SpecEventDefinitions.Mocks; } }
+    }
+}
