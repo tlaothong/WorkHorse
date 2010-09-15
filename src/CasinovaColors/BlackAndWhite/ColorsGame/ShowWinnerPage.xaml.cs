8884d933ca79cf295ad50342ba727bae484e63d2
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2010-09-15 15:44:34 +0700

    สร้าง ViewModels ShowWinnerPageViewModel เพื่อทำการแสดงผลการกดปุ่ม Get winner และเพิ่ม menu Silverlight ที่ Web Default
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- src/CasinovaColors/BlackAndWhite/ColorsGame/ShowWinnerPage.xaml.cs
+++ src/CasinovaColors/BlackAndWhite/ColorsGame/ShowWinnerPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using PerfEx.Infrastructure.Mvvm;
 
 namespace ColorsGame
 {
@@ -17,6 +18,7 @@ namespace ColorsGame
         public ShowWinnerPage()
         {
             InitializeComponent();
+            MvvmBinder.Bind(this, DataContext);
         }
     }
 }
