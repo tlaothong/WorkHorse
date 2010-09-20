8ed30f446a6af39403afc86dc4719e03f07d18b7
Teerachai <tlaothong@gmail.com>
2010-09-20 14:54:19 +0700

    Add ConceptSpike/MefConcept/MefFactory.
    Signed-off-by: Teerachai <tlaothong@gmail.com>

---
+++ src/ConceptSpike/MefConcept/MefFactory/SimpleFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+namespace MefFactory
+{
+    public class SimpleFactory
+    {
+        [ImportMany]
+        IEnumerable<Lazy<ExportClass, IFactoryMetadata>> Targets { get; set; }
+
+        public T GetObject<T>()
+            where T : ExportClass
+        {
+            return (T)Targets.Where(t => t.Metadata.TargetType.Equals(typeof(T))).Select(t => t.Value).FirstOrDefault();
+        }
+    }
+}
