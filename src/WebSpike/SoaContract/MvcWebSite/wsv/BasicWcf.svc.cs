6e2750d96c5080876bc4d95054d2a6ec63dda021
Teerachai <tlaothong@gmail.com>
2010-09-23 11:57:12 +0700

    Add SoaContract.
    Signed-off-by: Teerachai <tlaothong@gmail.com>

---
+++ src/WebSpike/SoaContract/MvcWebSite/wsv/BasicWcf.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MvcWebSite.Models.Services;
+
+namespace MvcWebSite.wsv
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BasicWcf" in code, svc and config file together.
+    public class BasicWcf : IBasicWcf
+    {
+        public void DoWork()
+        {
+        }
+
+        public Models.Services.BasicSimpleResponse BasicSimple(Models.Services.BasicSimpleRequest req)
+        {
+            return new BasicSimpleResponse {
+                Message = "RSP: " + req.Message,
+            };
+        }
+    }
+}
