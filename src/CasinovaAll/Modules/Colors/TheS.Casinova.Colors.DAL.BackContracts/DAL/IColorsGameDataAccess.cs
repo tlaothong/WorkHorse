84a8cfa3dacb7b1707ba1cd89ca42f3ab53d7c14
sirinarin <sirinarin@perfenterprise.com>
2010-09-27 16:23:00 +0700

    add BetCommand add IPayForColorsWinnerInfo
    Signed-off-by: sirinarin <sirinarin@perfenterprise.com>

---
+++ src/CasinovaAll/Modules/Colors/TheS.Casinova.Colors.DAL.BackContracts/DAL/IColorsGameDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheS.Casinova.Colors.Models;
+using TheS.Casinova.Colors.Commands;
+using PerfEx.Infrastructure.Data;
+
+namespace TheS.Casinova.Colors.DAL
+{
+    public interface IColorsGameDataAccess
+        : IPayForColorsWinnerInfo
+    { }
+
+    /// <summary>
+    /// หักเงินผู้เล่นค่าดูข้อมูลผู้ชนะในเวลานั้น
+    /// </summary>
+    public interface IPayForColorsWinnerInfo
+        : IDataAction<PlayerInformation, PayForColorsWinnerInfoCommand>
+    { }
+}
