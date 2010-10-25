01a037b4865e9eb22370173659a57aa51057dad4
Nittaya <nittaya@perfenterprise.com>
2010-10-25 15:06:44 -0700

    วางโครงของ CreatePlayerAccount, CancelPlayerAccount, EditPlayerAccount, ListPlayerAccount, GetPlayerAccount
    Signed-off-by: Nittaya <nittaya@perfenterprise.com>

--- src/CasinovaAll/Modules/PlayerAccount/TheS.Casinova.PlayerAccount.Commands/Commands/CreatePlayerAccountCommand.cs
+++ src/CasinovaAll/Modules/PlayerAccount/TheS.Casinova.PlayerAccount.Commands/Commands/CreatePlayerAccountCommand.cs
@@ -5,7 +5,36 @@ using System.Text;
 
 namespace TheS.Casinova.PlayerAccount.Commands
 {
+    /// <summary>
+    /// command สร้างข้อมูลบัญชีของผู้เล่น
+    /// </summary>
     public class CreatePlayerAccountCommand
     {
+        //input
+        /// <summary>
+        /// ชื่อผู้เล่น
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// ประเภทบัญชี
+        /// </summary>
+        public string AccountType { get; set; }
+
+        /// <summary>
+        /// หมายเลขบัญชี
+        /// </summary>
+        public int AccountNo { get; set; }
+
+        /// <summary>
+        /// รหัสตรวจสอบหมายเลขบัญชี
+        /// </summary>
+        public int CVV { get; set; }
+
+        /// <summary>
+        /// วันหมดอายุของบัญชี
+        /// </summary>
+        public DateTime ExpireDate { get; set; }
+
     }
 }
---
+++ src/CasinovaAll/Modules/PlayerAccount/TheS.Casinova.PlayerAccount.WebExecutors/WebExecutors/CreatePlayerAccountExecutor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PerfEx.Infrastructure.CommandPattern;
+using TheS.Casinova.PlayerAccount.Commands;
+using TheS.Casinova.PlayerAccount.BackServices;
+
+namespace TheS.Casinova.PlayerAccount.WebExecutors
+{
+    /// <summary>
+    /// สร้างบัญชีของผู้เล่น
+    /// </summary>
+    public class CreatePlayerAccountExecutor
+    : SynchronousCommandExecutorBase<CreatePlayerAccountCommand>
+    {
+        private ICreatePlayerAccount _iCreatePlayerAccount;
+
+        public CreatePlayerAccountExecutor(IPlayerAccountModuleBackService dac)
+        {
+            _iCreatePlayerAccount = dac;
+        }
+
+        protected override void ExecuteCommand(CreatePlayerAccountCommand command)
+        {
+            _iCreatePlayerAccount.CreatePlayerAccount(command);
+        }
+    }
+}
