04d9a1769a866389f4742255df6aad7633fa9cff
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2010-10-27 17:18:25 +0700

    edit Logout hyperlink button navigation to "GameLobbyPage" resize "GamePlayUI" from colors fix bug navigation

--- src/CasinovaAll/Silverlights/CasinovaAllStars/MainPage.xaml.cs
+++ src/CasinovaAll/Silverlights/CasinovaAllStars/MainPage.xaml.cs
@@ -25,21 +25,22 @@ namespace CasinovaAllStars
         // After the Frame navigates, ensure the HyperlinkButton representing the current page is selected
         private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
         {
-            foreach (UIElement child in LinkListBox.Items)
-            {
-                HyperlinkButton hb = child as HyperlinkButton;
-                if (hb != null && hb.NavigateUri != null)
-                {
-                    if (hb.NavigateUri.ToString().Equals(e.Uri.ToString()))
-                    {
-                        VisualStateManager.GoToState(hb, "ActiveLink", true);
-                    }
-                    else
-                    {
-                        VisualStateManager.GoToState(hb, "InactiveLink", true);
-                    }
-                }
-            }
+            //try {
+            //    foreach (UIElement child in LinkListBox.Items) {
+            //        HyperlinkButton hb = child as HyperlinkButton;
+            //        if (hb != null && hb.NavigateUri != null) {
+            //            if (hb.NavigateUri.ToString().Equals(e.Uri.ToString())) {
+            //                VisualStateManager.GoToState(hb, "ActiveLink", true);
+            //            }
+            //            else {
+            //                VisualStateManager.GoToState(hb, "InactiveLink", true);
+            //            }
+            //        }
+            //    }
+            //}
+            //catch (Exception ex) {
+            //    MessageBox.Show(ex.ToString());
+            //}
         }
 
         // If an error occurs during navigation, show an error window
--- src/CasinovaAll/Silverlights/CasinovaAllStars/ViewModels/MainViewModel.cs
+++ src/CasinovaAll/Silverlights/CasinovaAllStars/ViewModels/MainViewModel.cs
@@ -11,31 +11,46 @@ using System.Windows.Shapes;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
 using TheS.Casinova.Common;
+using System.Collections.Specialized;
 
 namespace CasinovaAllStars.ViewModels
 {
     // TODO : Error navigation when using this viewmodel
     public class MainViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<Lazy<ChildWindow, IPopupContentMetadata>> _popups;
+        private ReadOnlyObservableCollection<Lazy<ChildWindow, IPopupContentMetadata>> _readOnlyPopups;
+
         public MainViewModel()
         {
             if (App.SupportContentDownloaded == false)
             {
-                //App.SupportContentDownloadCompleted += new EventHandler(App_SupportDownloadedCompleted);
+                App.SupportContentDownloadCompleted += new EventHandler(App_SupportContentDownloadCompleted);
+            }
+            _popups = new ObservableCollection<Lazy<ChildWindow, IPopupContentMetadata>>();
+            _readOnlyPopups = new ReadOnlyObservableCollection<Lazy<ChildWindow, IPopupContentMetadata>>(_popups);
+        }
+
+        void App_SupportContentDownloadCompleted(object sender, EventArgs e)
+        {
+            ((INotifyCollectionChanged)App.ModuleLoader.PopupContents).CollectionChanged += new NotifyCollectionChangedEventHandler(MainViewModel_CollectionChanged);
+            PropertyChangedEventHandler temp = PropertyChanged;
+            if (temp != null) {
+                temp(this, new PropertyChangedEventArgs("Popups"));
+            }
+        }
+
+        void MainViewModel_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            _popups.Clear();
+            foreach (var item in App.ModuleLoader.PopupContents) {
+                _popups.Add(item);
             }
         }
 
         public ReadOnlyObservableCollection<Lazy<ChildWindow, IPopupContentMetadata>> Popups
         {
-            get { return App.ModuleLoader.PopupContents; }
-        }
-
-        private void App_SupportDownloadedCompleted(object sender, EventArgs e)
-        {
-            PropertyChangedEventHandler temp = PropertyChanged;
-            if (temp != null) {
-                temp(this, new PropertyChangedEventArgs("Popups"));
-            }
+            get { return _readOnlyPopups; }
         }
 
         #region INotifyPropertyChanged Members
