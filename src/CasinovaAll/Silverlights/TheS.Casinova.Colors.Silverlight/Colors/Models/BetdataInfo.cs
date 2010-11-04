3e2b7f37b8ed74f644902680c165f487625750a1
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2010-11-04 19:33:05 +0700

    add GameStatistics and ViewModel for games Colors, DevilSix, MagicNine, TwoWins
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ src/CasinovaAll/Silverlights/TheS.Casinova.Colors.Silverlight/Colors/Models/BetdataInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TheS.Casinova.Colors.Models
+{
+    public class BetdataInfo
+    {
+        public DateTime Time { get; set; }
+        public int Round { get; set; }
+        public string Player { get; set; }
+        public double Bet { get; set; }
+        public string Color { get; set; }
+        public string WinColor { get; set; }
+        public bool Match { get; set; }
+    }
+}
