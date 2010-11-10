11acc4edf042e54c9848540ae3b501f3817f2580
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2010-11-10 11:16:35 +0700

    add user interface chart bar/line to games TwoWins, DevilSix, MagicNine, Colors add ViewModels for chart all + sample data in design view
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- src/CasinovaAll/Silverlights/TheS.Casinova.TwoWins.Silverlight/TwoWins/ViewModels/BetdataViewModel.cs
+++ src/CasinovaAll/Silverlights/TheS.Casinova.TwoWins.Silverlight/TwoWins/ViewModels/BetdataViewModel.cs
@@ -11,6 +11,7 @@ using System.Windows.Shapes;
 using TheS.Casinova.TwoWins.Models;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace TheS.Casinova.TwoWins.ViewModels
 {
@@ -20,11 +21,66 @@ namespace TheS.Casinova.TwoWins.ViewModels
         
         private PerfEx.Infrastructure.PropertyChangedNotifier _notify;
         private ObservableCollection<BetdataInfo> _informations;
+        private ObservableCollection<KeyValuePair<DateTime, double>> _lineInformations;
+        private ObservableCollection<KeyValuePair<DateTime, double>> _barInformations;
+        private string _chartName;
+        private double _maximum;
+        private double _interval;
 
         #endregion Fields
 
         #region Properties
-        
+
+        public double Interval
+        {
+            get { return _interval; }
+            set
+            {
+                _interval = value;
+                _notify.Raise(() => Interval);
+            }
+        }
+
+        public double Maximum
+        {
+            get { return _maximum; }
+            set
+            {
+                _maximum = value;
+                _notify.Raise(() => Maximum);
+            }
+        }
+
+        public string ChartName
+        {
+            get { return _chartName; }
+            set
+            {
+                _chartName = value;
+                _notify.Raise(() => ChartName);
+            }
+        }
+
+        public ObservableCollection<KeyValuePair<DateTime, double>> BarInformations
+        {
+            get { return _barInformations; }
+            set
+            {
+                _barInformations = value;
+                _notify.Raise(() => BarInformations);
+            }
+        }
+
+        public ObservableCollection<KeyValuePair<DateTime, double>> LineInformations
+        {
+            get { return _lineInformations; }
+            set
+            {
+                _lineInformations = value;
+                _notify.Raise(() => LineInformations);
+            }
+        }
+
         public ObservableCollection<BetdataInfo> Informations
         {
             get { return _informations; }
@@ -43,8 +99,31 @@ namespace TheS.Casinova.TwoWins.ViewModels
         {
             _notify = new PerfEx.Infrastructure.PropertyChangedNotifier(this, () => PropertyChanged);
             _informations = new ObservableCollection<BetdataInfo>();
+            _lineInformations = new ObservableCollection<KeyValuePair<DateTime, double>>();
+            _barInformations = new ObservableCollection<KeyValuePair<DateTime, double>>();
 
             if (DesignerProperties.IsInDesignTool) {
+
+                ChartName = "TwoWins (102-351)";
+
+                Random random = new Random();
+                DateTime time = DateTime.Now;
+                double pot = 0;
+                for (int count = 0; count < 40; count++)
+                {
+                    var data = new KeyValuePair<DateTime, double>(time, pot);
+                    LineInformations.Add(data);
+                    BarInformations.Add(data);
+
+                    if (Maximum < pot)
+                    {
+                        Maximum = pot;
+                    }
+                    pot += count * random.Next(1, 53);
+                    time = time.Add(TimeSpan.FromMinutes(18));
+                }
+                Interval = (int)(Maximum / 5);
+
                 Informations.Add(new BetdataInfo {
                     Player = "John",
                     GameState = "Normal",
