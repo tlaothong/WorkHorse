a5cbde790fa102fc02d02241c03b6350551d3e59
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2010-11-23 10:54:12 +0700

    Add MagicNineServiceAdapter and interface to game magic 9
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ src/CasinovaAll/Silverlights/TheS.Casinova.MagicNine.Silverlight/MagicNine/Services/IMagicNineServiceAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TheS.Casinova.MagicNineSvc;
+
+namespace TheS.Casinova.MagicNine.Services
+{
+    public interface IMagicNineServiceAdapter
+    {
+        IObservable<ListActiveGameRoundInfoCommand> GetListActiveGameRound();
+
+        IObservable<ListGamePlayAutoBetInfoCommand> GetListGamePlayAutoBetInformation(ListGamePlayAutoBetInfoCommand cmd);
+
+        IObservable<ListBetLogCommand> GetListBetLog(ListBetLogCommand cmd);
+
+        IObservable<SingleBetCommand> BetSingle(SingleBetCommand cmd);
+
+        IObservable<StartAutoBetCommand> AutoBetOn(StartAutoBetCommand cmd);
+
+        IObservable<StopAutoBetCommand> AutoBetOff(StopAutoBetCommand cmd);
+    }
+}
